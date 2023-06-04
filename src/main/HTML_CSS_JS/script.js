7ad9635d0383c24071590b4398688b1a1455292a
cristina.popenta <cristina.popenta@gmail.com>
2023-06-04 15:01:51 +0300

    Course02 homework commit with CSS and JS

--- src/main/HTML_CSS_JS/script.js
+++ src/main/HTML_CSS_JS/script.js
@@ -1,19 +1,52 @@
 function clickMe(){
-    var var1 = "abc";
-    var var2 = 5;
-    var var3 = false;
+    //var var1 = "abc";
+    //var var2 = 5;
+    //var var3 = false;
     //alert("hello!!!"); //existing fc in JS; alert, prompt and confirm
-    readInputText();
+    hitButton();
+}
+
+function hitButton(){
+    //alert("test alert");
+    //prompt("test prompt");
+    confirm("test confirm");
 }
 
 function readInputText(){
     var usernameText=document.getElementById("username").value; //username e id ul din html
     //alert(usernameText);
     document.getElementById("result").innerHTML = "the username is " + usernameText;
-    if (usernameText.length > 10){
-        alert("max size of 10 has been exceeded");
-        document.getElementById("result").style.color="red";
-    }
+    if (usernameText.length > 10) {
+        alert("max size of 10 has been exceeded; try again with less than 10 characters");
+        document.getElementById("username").style.color="red";
+        //document.getElementById("username").value='';
+        usernameText=document.getElementById("username").value;
+    };
 }
 
-//validari pe campuri 5 cu js; si pagina mai frumoasa, cu css
+function readPassword(){
+    var usernamePass=document.getElementById("pass").value;
+    if (usernamePass.length < 6){
+        alert("password too short; try again with more than 6 characters");
+    }
+}  // function was written in case it is used separatley; read and repeat password are tested together
+
+function repeatPassword(){
+    var usernamePassRepeat=document.getElementById("passrepeat").value;
+} //function was written in case it is used separatley; read and repeat password are tested together
+
+
+function validateUserPass()
+{
+    var usernamePass=document.getElementById("pass").value;
+    var usernamePassRepeat=document.getElementById("passrepeat").value;
+    if (usernamePass.length < 6){
+            alert("password too short");
+    }
+    if (usernamePass != usernamePassRepeat ){
+            alert("password mismatch!!!!!");
+    }
+}   // fct displays proper messages individually in case one test fails, and both messages in case both tests fail
+
+
+//tema: validari pe campuri cu js; si pagina mai frumoasa, cu css
