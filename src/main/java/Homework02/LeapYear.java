5599e092dab1e51027217292b7ad3c0ed483aa10
cristina.popenta <cristina.popenta@gmail.com>
2023-06-12 09:31:31 +0300

    class and file re-namining from Leap to LeapYear

--- src/main/java/Homework02/LeapYear.java
+++ src/main/java/Homework02/LeapYear.java
@@ -1,7 +1,7 @@
 package Homework02;
 import java.util.Scanner;
 
-public class Leap {
+public class LeapYear {
     public static void main(String[] args){
 
         Scanner input = new Scanner (System.in);
